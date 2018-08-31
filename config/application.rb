cf1ad385875774e24469d1246e54ed4bf5e12299
Reinaldo Vianna <reinaldo.vns@gmail.com>
2018-08-31 10:37:21 -0300

    add config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
 
 module Finance
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
